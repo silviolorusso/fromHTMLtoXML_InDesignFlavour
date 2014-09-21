44b20cd4e1835444cd26fce278411ed19ac37add
roberto-arista <arista.rob@gmail.com>
2014-09-21 13:41:02 +0200

    Indent spaces fixed

--- main.py
+++ main.py
@@ -13,36 +13,36 @@ from footnotes import format_fn
 # FUNCTIONS
 
 def main():
-  # open file and read html
-  try:
-    src = str(sys.argv[1])
-  except IndexError:
-    print '\nNo file provided. Usage: main.py filename\n'
-    return
-  f = open(src, 'r+')
-  html = f.read()
-  # if there's a body, extract its content, otherwise use it as it is
-  soup = BeautifulSoup(html)
-  body = ''
-  if soup.body:
-    # a bit annoying that you loose indents and you get comments
-    for t in soup.body.contents:
-      body += str(t)
-      xml = body
-  else:
-    xml = str(soup)
+	# open file and read html
+	try:
+		src = str(sys.argv[1])
+	except IndexError:
+		print '\nNo file provided. Usage: main.py filename\n'
+		return
+	f = open(src, 'r+')
+	html = f.read()
+	# if there's a body, extract its content, otherwise use it as it is
+	soup = BeautifulSoup(html)
+	body = ''
+	if soup.body:
+		# a bit annoying that you loose indents and you get comments
+		for t in soup.body.contents:
+			body += str(t)
+			xml = body
+	else:
+		xml = str(soup)
 
-  # do stuff
+	# do stuff
 
-  # clean linebreaks
-  xml = clean_linebreaks(xml)
+	# clean linebreaks
+	xml = clean_linebreaks(xml)
 
-  # format footnotes
-  xml = format_fn(xml)
+	# format footnotes
+	xml = format_fn(xml)
 
-  return xml
+	return xml
 
 # TEST
 
 if __name__ == '__main__':
-  print(main()) 
+	print(main()) 
